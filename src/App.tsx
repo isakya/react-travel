758359cbc132befdbdf0a3fb0de649125c1568f5
jinkun <1016361268@qq.com>
2022-10-13 19:37:01 +0800

    配置基础路由系统

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './App.module.css'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { HomePage } from './pages';
 
 
@@ -9,7 +9,12 @@ function App() {
   return (
     <div className={styles.App}>
       <BrowserRouter>
-        <Route path="/" component={HomePage} />
+        {/* Switch 每次只匹配一条路由，相同路由则匹配代码最前面的一条，作用是防止页面堆叠 */}
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/signIn" render={() => <h1>登录页面</h1>} />
+          <Route render={() => <h1>404 not found 页面去火星了 ！</h1>} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
