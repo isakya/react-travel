a962a108c9ba327428a57aaac60adae6c4bc5fc7
jinkun <1016361268@qq.com>
2022-10-19 17:19:52 +0800

    使用react-router-dom的Link标签实现跳转页面

--- src/components/productCollection/ProductImage.tsx
+++ src/components/productCollection/ProductImage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Image, Typography } from 'antd'
-import { withRouter, RouteComponentProps } from 'react-router-dom'
+import { withRouter, RouteComponentProps, Link } from 'react-router-dom'
 
 interface PropsType extends RouteComponentProps {
   id: string | number
@@ -21,7 +21,7 @@ const ProductImageComponent: React.FC<PropsType> = ({
   match
 }) => {
   return (
-    <div onClick={() => history.push(`/detail/${id}`)}>
+    <Link to={`detail/${id}`}>
       {
         size === 'large' ? (<Image src={imageSrc} height={285} width={490} />) : (<Image src={imageSrc} height={120} width={240} />)
       }
@@ -29,7 +29,7 @@ const ProductImageComponent: React.FC<PropsType> = ({
         <Typography.Text type="secondary">{title.slice(0, 25)}</Typography.Text>
         <Typography.Text type="danger" strong>￥ {price} 起</Typography.Text>
       </div>
-    </div>
+    </Link>
   )
 }
 
